6679c0afcff2f4049326b7def99d6d3ef4f1d6b6
Orny Nabila <168272886+orny1036@users.noreply.github.com>
2025-11-11 12:32:38 +0600

    You are given 2 non-empty strings 's1' and 's2' consisting of lowercase English alphabets only.    In one operation you can do either of the following on 's1':  (1) Remove a character from any position in 's1'.  (2) Add a character to any position in 's1'.    Find the minimum number of operations required to convert string 's1' into 's2'.

---
+++ stringMatching/minimum_insertions_deletions.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int canYouMake(string &s1, string &s2){
+   
+   int n = s1.size();
+   int m = s2.size();
+
+   vector<int>prev(m+1, 0), cur(m+1, 0);
+
+   for(int i=1 ; i<=n; ++i)
+   {
+       for(int j = 1; j<=m; ++j)
+       {
+           if(s1[i-1]==s2[j-1])
+           {
+               cur[j] = 1 + prev[j-1];
+           }
+           else
+           {
+               cur[j] =  max(prev[j], cur[j-1]);
+           }
+       }
+       prev = cur;
+   }
+
+   int len = prev[m];
+
+   int deletions = n - len;
+   int insertions = m - len;
+
+   return deletions + insertions;
+}
